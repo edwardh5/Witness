ad551199b4a26934a7b8943c58087e7bc41fe1d7
Edward He <edward.he@macaulay.cuny.edu>
2017-10-09 16:55:01 -0400

    Refactor and add modifier.

--- contracts/Witness.sol
+++ contracts/Witness.sol
@@ -8,51 +8,51 @@ contract Witness {
   }*/
 
   struct User {
-    address userAddress;
     bytes32 username;
     bytes32[] posts;
+    bool exists;
   }
 
   mapping (address => User) public users;
+  mapping (bytes32 => bool) public usernames;
 
   function Witness() {
     owner = msg.sender;
   }
 
-//  modifier ensureNewUser(bytes32 _username) {
-//    if (users[msg.sender] == '0x') {
-//      /*there's already a user associated with this address*/
-//    }
-//    else if (/* check if username is already taken */) {
-//      /*Username is already in use, please try a different username*/
-//    }
-//  }
-
-  function getUser(address _userAddress) constant returns(bytes32) {
-    return users[_userAddress].username;
+  modifier ensureNewUser(bytes32 _username) {
+    require(
+      users[msg.sender].exists != true &&
+      usernames[_username] != true
+    );
+    _;
   }
 
-  function getPostsForUser(address _userAddress) constant returns(bytes32[]) {
-    return users[_userAddress].posts;
+  function getUser(address _userAddress) constant returns(bytes32, bytes32[]) {
+    return (
+      users[_userAddress].username,
+      users[_userAddress].posts
+    );
   }
 
-  function createNewUser(bytes32 _username) returns(bool) {
+  function createNewUser(bytes32 _username) ensureNewUser(_username) returns(bool) {
     bytes32[] memory newPosts;
 
     User memory newUser = User({
-      userAddress: msg.sender,
       username: _username,
-      posts: newPosts
+      posts: newPosts,
+      exists: true
     });
+
+    /*There's probably a better/less expensive way way to check for existence of a username*/
+    usernames[_username] = true;
 
     users[msg.sender] = newUser;
     return true;
   }
 
   function createNewPost(bytes32 _body) returns(bool) {
-    /*bytes32 memory newPost = _body;*/
-
     users[msg.sender].posts.push(_body);
-    returns true;
+    return true;
   }
 }
