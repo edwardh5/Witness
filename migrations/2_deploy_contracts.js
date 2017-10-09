9681c5a9365268e722d41cbbdfb35f7addeecaba
Edward He <edward.he@macaulay.cuny.edu>
2017-10-09 18:25:23 -0400

    Include React with Redux and Auth.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
+var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
+var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
+var Authentication = artifacts.require("./Authentication.sol");
+
 var Witness = artifacts.require("./Witness.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Ownable);
+  deployer.link(Ownable, Killable);
+  deployer.deploy(Killable);
+  deployer.link(Killable, Authentication);
+  deployer.deploy(Authentication);
+  
   deployer.deploy(Witness);
 };
